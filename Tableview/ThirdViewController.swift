51537215b2b6fe4942a70da58c9f0230b32cf211
irinejiss <87313540+irinejiss@users.noreply.github.com>
2021-08-11 12:38:11 +0530

    Constraints added

--- Tableview/ThirdViewController.swift
+++ Tableview/ThirdViewController.swift
@@ -8,34 +8,25 @@
 import UIKit
 import TextFieldEffects
 import SkyFloatingLabelTextField
-class ThirdViewController: UIViewController {
-    @IBOutlet weak var mainTitle: UILabel!
 
+class ThirdViewController: UIViewController,UITextFieldDelegate,UITextViewDelegate{
+    
+    @IBOutlet weak var mainTitle: UILabel!
+    @IBOutlet weak var setAddress: UITextView!
+  
     var userName:String?
+    var textView = ""
     override func viewDidLoad() {
         super.viewDidLoad()
+        setAddress.delegate = self
+        setAddress.text = "Address"
+        setAddress.textColor = UIColor.lightGray
         setName()
-        giveName()
-        giveNum()
+    
     }
     func setName() {
         guard let name = userName else { return }
         mainTitle.text = name
        
     }
-    func giveName(){
-        let textField = AkiraTextField(frame:CGRect(x: 30, y: 200, width: 300, height: 40))
-        _ = UIColor(red: 197/255, green: 205/255, blue: 205/255, alpha: 1.0)
-        _ = UIColor(red: 52/255, green: 42/255, blue: 61/255, alpha: 1.0)
-        textField.placeholder = "Name"
-        textField.placeholderColor = .darkGray
-//        textField.foregroundColor = .lightGrayColor()
-
-        self.view.addSubview(textField)
-    }
-    func giveNum(){
-        let numField = SkyFloatingLabelTextField(frame: CGRect(x: 40, y: 300, width: 200, height: 45))
-        numField.placeholder = "Ph.No"
-        self.view.addSubview(numField)
-}
 }
